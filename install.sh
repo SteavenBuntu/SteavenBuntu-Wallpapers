ea535eef9abc2a0bffebf40b8bcbdd70c9c4ab85
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-03-24 00:38:12 +0200

    ah

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/SteavenBuntu/SteavenBuntu-Wallpapers 
 cd SteavenBuntu-Wallpapers 
-cd PNG
-sudo cp *.png /usr/share/backgrounds
+cd JPG
+sudo cp *.jpg /usr/share/backgrounds
 cd ..
 cd SteavenBuntu-Wallpapers 
 sudo cp wallpapers.xml /usr/share/gnome-background-properties/
